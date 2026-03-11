ea8c072da65c8aac838fcde0e7368d5a9f768197
cunderhill17 <93687857+cunderhill17@users.noreply.github.com>
2026-03-11 19:16:47 -0400

    Section one
    add base URL to api, add books index api route for listing books, add show route for books (shows single book),

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::get('/', function() {
+    return json_encode(['hello' => 'world']);
+});
+
+// ::class prints the FQN, which is App\Http\Controllers\BookController
+Route::get('/books', [BookController::class, 'index']);
+
+Route::get('/books/{book}', [BookController::class, 'show']);
